7a8eb8160e3baa0ff946d4b28b41b6c3d89cb287
Jeremy Capello <me@lethargicdeveloper.com>
2023-10-27 10:48:47 -0500

    added pipe puzzle

---
+++ src/Scenes/Mansion/pipe-static.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+
+@onready var MeshInstance := $".."
+
+var interacted := false
+
+func _process(delta: float) -> void:
+	if interacted:
+		interacted = false
+		MeshInstance.rotation_degrees.y += 90
+		if MeshInstance.rotation_degrees.y == 360:
+			MeshInstance.rotation_degrees.y = 0
+
+func can_interact() -> bool:
+	return true
+
+func player_interact() -> void:
+	interacted = true
+	
