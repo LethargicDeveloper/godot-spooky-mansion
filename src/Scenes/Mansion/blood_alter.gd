10b72b9edf66ac0ba897ee1e73cb86b742ac9484
Jeremy Capello <me@lethargicdeveloper.com>
2023-11-02 15:35:34 -0500

    added fill blod animation

---
+++ src/Scenes/Mansion/blood_alter.gd
@@ -0,0 +1,15 @@
+extends StaticBody3D
+
+@onready var parent = $".."
+@onready var BloodPool = $"../../BloodAlter_Blood"
+@onready var player = $"../../AnimationPlayer"
+
+var activated = false
+
+func can_interact() -> bool:
+	return !activated
+
+func player_interact() -> void:
+	if (!activated):
+		activated = true
+		player.play("fill_blood")
